4fe3f8aae3c581c90e582a06e1ec983ab98f863e
snake.lin <snake.lin@ly.com>
2024-05-26 17:57:18 +0800

    add theme control

--- client/AI.Chat.Copilot/Controls/GlobalSettings.axaml.cs
+++ client/AI.Chat.Copilot/Controls/GlobalSettings.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using SukiUI;
 
 namespace AI.Chat.Copilot;
 
@@ -9,5 +11,20 @@ public partial class GlobalSettings : UserControl
     public GlobalSettings()
     {
         InitializeComponent();
+    }
+
+    private void RadioButton_Checked_1(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Light);
+    }
+
+    private void RadioButton_Checked_2(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Dark);
+    }
+
+    private void BtnColor_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SukiTheme.GetInstance().SwitchColorTheme();
     }
 }
